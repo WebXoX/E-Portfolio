bfe73092bb5d7705128e132fe18f9d5d8186640b
Joe <97600250+WebXoX@users.noreply.github.com>
2023-02-24 01:56:27 +0400

    final

---
+++ js/projects.js
@@ -0,0 +1,55 @@
+let project = [
+    
+     {
+        "id": 1001,
+        "title": "Piscine",
+        "discription":"The 42 Piscine is a 4-week intensive programming bootcamp offered by the 42 school, where students learn the fundamentals of computer programming, algorithmic problem solving, and software development through a series of hands-on projects. The program covers various programming languages, including C, and shell scripting.",
+        "link": "https://github.com/WebXoX/piscine",
+        "image": "../img/42piscine.png",        
+        "image2": "../img/swim.png",
+        "show":true
+    },{
+        "id": 1002,
+        "title": "Libft",
+        "discription":"LIBFT is a project created for students of the 42 coding school, which involves recreating existing C library functions as well as implementing new ones. The goal of the project is to help students gain a deeper understanding of standard C library functions and to improve their programming skills. The project involves writing functions that perform common operations such as string manipulation, memory allocation, and basic data type operations. Some of the functions that are implemented include strlen, strcpy, strcmp, atoi, and malloc, among others. In addition, students are required to create a library that contains all of the implemented functions, which can be used in future projects.The LIBFT project is considered a fundamental project for students of 42, as it provides a foundation for their programming skills and helps them to understand the inner workings of standard C library functions.",
+        "link": "https://github.com/WebXoX/LIBFT",
+        "image": "../img/libft.png",        
+        "image2": "../img/c.png",
+        "show":true
+    },
+     {
+        "id": 1003,
+        "title": "Get_Next_Line",
+        "discription":"The get_next_line project in 42 teaches several important skills and concepts related to file input/output and memory management in C programming. Firstly, it teaches how to read input from a file and return a line of input each time the function is called, using various techniques such as buffer management and string manipulation. This involves understanding the concepts of file descriptors, memory allocation, and pointer manipulation. Additionally, the project emphasizes the importance of error handling, and requires the implementation of error checking and handling to ensure the program is robust and does not crash when encountering unexpected input or errors. Overall, the get_next_line project is a good introduction to file input/output in C, and provides a foundation for more advanced programs that require reading and processing data from files.",
+        "link": "https://github.com/WebXoX/get_next_line",
+        "image": "../img/Get_next_line.png",        
+        "image2": "../img/c.png",
+        "show":true
+    },
+    {
+        "id": 1004,
+        "title": "Ft_Printf",
+        "discription":"The ft_printf project in 42 teached me how to recreate the functionality of the standard C library function printf(). Students learn to parse the format string, handle various format specifiers, and output the formatted text to the console or a file.The project involves creating a custom version of printf() called ft_printf() that accepts a format string and any additional arguments, then outputs the formatted text. Students must handle various format specifiers, such as %d for integers, %s for strings, and %f for floating point numbers, and be able to format the output based on the specifier and any additional flags or width/precision information provided in the format string. Through this project, I gained a deeper understanding of string manipulation, memory management, and the inner workings of the standard C library function printf(). They also learn to write efficient and reusable code, as well as develop debugging skills to find and fix errors in their code.",
+        "link": "https://github.com/WebXoX/FT_PRINTF",
+        "image": "../img/ft_printf.png",        
+        "image2": "../img/c.png",
+        "show":true
+    },
+    {
+        "id": 1005,
+        "title": "Node",
+        "discription":"In this project I learned node.js, express, and SQL in order to build a single page application(A blog website). Through this project, I gained a deeper understanding of the structure and functionality of web applications, particularly with regards to the backend. By working with these technologies, I was able to create a robust and dynamic application that can handle user input, store data in a database, and retrieve and display that data to the user. Additionally, I learned how to work with RESTful APIs, which are used to enable communication between different parts of a web application. Overall, this project provided me with valuable experience and skills in web development and software engineering.",
+        "link": "https://github.com/WebXoX/node",
+        "image": "../img/node.png",        
+        "image2": "../img/nodejs.png",
+        "show":true
+    }, {
+        "id": 1006,
+        "title": "Traveling Salesman Problem",
+        "discription":"I learned how to solve the traveling salesman problem using the 2-opt algorithm in Java. The 2-opt algorithm is a local search algorithm that iteratively improves a given tour of cities until a locally optimal solution is reached. The worst part of this process was preparing the data, which involves creating a distance matrix for the cities to be visited. The best part of this process was search, where I was able to apply the 2-opt algorithm to iteratively improve the tour and reach a locally optimal solution. Overall, this gave me a solid foundation in problem-solving with algorithms and data structures, as well as experience working with Java programming language. ",
+        "link": "https://github.com/WebXoX/Traveling-Sales-Man",
+        "image": "../img/tsm.png",        
+        "image2": "../img/java.png",
+        "show":true
+    }
+]
---
+++ js/skills.js
@@ -0,0 +1,58 @@
+let skill = [
+    
+    {
+        "title": "C",
+        "discription":"I have a deep understanding of its syntax, data types, and memory management. I am comfortable working with pointers, arrays, structures, and functions, and can write efficient and optimized code. I also have experience with low-level programming and can work with system calls and machine-level code. Overall, my expertise in C allows me to develop high-performance applications and software solutions with confidence.",
+        "image": "../img/C.png",
+    },
+    {
+        "title": "JAVA",
+        "discription":" I am comfortable using the language to develop complex applications and solutions. I am knowledgeable about object-oriented programming concepts, such as inheritance, polymorphism, and encapsulation. I have experience working with various frameworks and libraries, including Spring and Hibernate, and I am familiar with development tools such as Eclipse and IntelliJ IDEA. Overall, I feel confident in my ability to tackle challenging projects and deliver high-quality solutions using Java.",
+        "image": "../img/java.png",
+    },
+    {
+        "title": "HTML5",
+        "discription":"I can confidently say that I have an expert understanding of the language. I am very familiar with the semantic elements introduced in HTML5, and I use them to create clean and accessible markup for websites and web applications. I am also well-versed in HTML5 APIs, such as the canvas and video APIs, which I use to create engaging and interactive user experiences. Additionally, I have experience working with various HTML5 frameworks and libraries, which has allowed me to streamline my development process and create high-quality code efficiently.",
+        "image": "../img/html.png",
+    },
+    {
+        "title": "CSS",
+        "discription":" I feel confident in my ability to design and style websites in a professional manner. I have a strong understanding of CSS syntax, including the use of selectors, properties, and values. I am comfortable using CSS preprocessors such as SASS or LESS to streamline my workflow and improve maintainability of my code. Additionally, I have experience with responsive design, using media queries and other techniques to ensure that websites display properly on a variety of devices and screen sizes.",
+        "image": "../img/css.png",
+    },
+    {
+        "title": "JavaScript",
+        "discription":"I feel confident working with both frontend and backend web development. I am familiar with modern JavaScript frameworks such as Vue.js and React, and am comfortable using libraries like jQuery and Lodash. I have a deep understanding of core concepts such as closures, prototypes, and the event loop, which allows me to write optimized and efficient code. Additionally, I have experience working with server-side JavaScript platforms like Node.js, and am comfortable building RESTful APIs using popular frameworks like Express.js. Overall, my expertise in JavaScript enables me to create dynamic, responsive, and interactive web applications.",
+        "image": "../img/js.png",
+    },
+    {
+        "title": "Node.js",
+        "discription":" I have used it extensively in my web development projects. I am comfortable with the various Node modules and have experience building RESTful APIs with Express.js. I have also worked with popular Node.js frameworks like Meteor.js and Nest.js. Additionally, I have expertise in integrating Node.js with databases like MongoDB and MySQL, and have used Node.js for real-time communication using WebSockets.",
+        "image": "../img/nodejs.png",
+    },
+    {
+        "title": "Vue.js",
+        "discription":" I am comfortable using Vue.js to create both single-page and multi-page applications, and have experience integrating it with various back-end technologies. I am also proficient in using Vue.js with other popular libraries and frameworks, such as Vuex and Vue Router, to build complex and robust applications.",
+        "image": "../img/vue.png",
+    },
+    {
+        "title": "Express",
+        "discription":"I have a deep understanding of its core concepts, including routing, middleware, and templating engines. I have built a variety of web applications using Express, ranging from small APIs to complex full-stack applications. I am well-versed in utilizing third-party packages and libraries to enhance the functionality of my Express applications, and have experience working with databases and authentication in this framework. Overall, I am confident in my ability to build scalable and performant web applications using Express.js.",
+        "image": "../img/express.png",
+    },
+    {
+        "title": "Python",
+        "discription":" I have been working with the language for several years now. I have a strong understanding of the language's syntax and features, and have used it extensively for a variety of tasks, including data analysis, web scraping, and automation. I am proficient in popular Python libraries such as NumPy, Pandas, and Matplotlib, and have also contributed to open source projects written in Python.",
+        "image": "../img/python.png",
+    },
+    {
+        "title": "SQL",
+        "discription":" I am proficient in creating complex database queries, designing efficient database schemas, and optimizing SQL performance. I am also experienced in working with various database management systems like MySQL, PostgreSQL, and Oracle. Additionally, I have a strong understanding of advanced SQL concepts such as stored procedures, triggers, and views, and am able to use them effectively to build robust database solutions.",
+        "image": "../img/sql.png",
+    },
+    {
+        "title": "MongoDb",
+        "discription":"I can say that it is a powerful NoSQL database that allows for flexible and scalable data storage. With its document-oriented model and dynamic schema, it offers a lot of versatility in terms of data organization and querying. I am experienced in working with MongoDB in both development and production environments, and am well-versed in utilizing its various features and tools to create robust and efficient data-driven applications.",
+        "image": "../img/mongodb.png",
+    }
+]
