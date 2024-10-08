8991e9af53af15a9410eee6419f1343821f991f7
Joe <97600250+WebXoX@users.noreply.github.com>
2024-10-08 19:53:45 +0000

    fixing images and descriptions

--- js/projects.js
+++ js/projects.js
@@ -55,46 +55,60 @@ let project = [
     , {
         "id": 1007,
         "title": "Cub3D",
-        "discription":"I learned how to solve the traveling salesman problem using the 2-opt algorithm in Java. The 2-opt algorithm is a local search algorithm that iteratively improves a given tour of cities until a locally optimal solution is reached. The worst part of this process was preparing the data, which involves creating a distance matrix for the cities to be visited. The best part of this process was search, where I was able to apply the 2-opt algorithm to iteratively improve the tour and reach a locally optimal solution. Overall, this gave me a solid foundation in problem-solving with algorithms and data structures, as well as experience working with Java programming language. ",
-        "link": "https://github.com/WebXoX/Traveling-Sales-Man",
-        "image": "../img/tsm.png",        
-        "image2": "../img/java.png",
+        "discription":"cube3d is an exciting graphics project that challenges students to create a 3D maze-like game using raycasting techniques. This project is inspired by the classic game Wolfenstein 3D and aims to improve your skills in graphics programming, optimization, and game development.",
+        "link": "https://github.com/WebXoX/cube3d",
+        "image": "../img/cub3d.png",        
+        "image2": "../img/c.png",
         "show":true
     }, 
     {
         "id": 1008,
         "title": "Minishell",
-        "discription":"I learned how to solve the traveling salesman problem using the 2-opt algorithm in Java. The 2-opt algorithm is a local search algorithm that iteratively improves a given tour of cities until a locally optimal solution is reached. The worst part of this process was preparing the data, which involves creating a distance matrix for the cities to be visited. The best part of this process was search, where I was able to apply the 2-opt algorithm to iteratively improve the tour and reach a locally optimal solution. Overall, this gave me a solid foundation in problem-solving with algorithms and data structures, as well as experience working with Java programming language. ",
-        "link": "https://github.com/WebXoX/Traveling-Sales-Man",
-        "image": "../img/tsm.png",        
-        "image2": "../img/java.png",
+        "discription":" The 42 Minishell project explores system-level programming and Unix shell \
+                        basics. It involves creating a simple shell for user interaction through    \
+                        command-line inputs, covering fundamental concepts like process management, \
+                        signal handling, and input/output redirection. Developing Minishell includes    \
+                        mastering system calls like fork and execve, parsing user commands, managing    \
+                        environmental variables, and implementing basic shell features like piping and  \
+                        signal handling.",
+        "link": "https://github.com/WebXoX/42_minishell",
+        "image": "../img/minishell.png",        
+        "image2": "../img/c.png",
         "show":true
     },
     {
         "id": 1009,
         "title": "IRC",
-        "discription":"I learned how to solve the traveling salesman problem using the 2-opt algorithm in Java. The 2-opt algorithm is a local search algorithm that iteratively improves a given tour of cities until a locally optimal solution is reached. The worst part of this process was preparing the data, which involves creating a distance matrix for the cities to be visited. The best part of this process was search, where I was able to apply the 2-opt algorithm to iteratively improve the tour and reach a locally optimal solution. Overall, this gave me a solid foundation in problem-solving with algorithms and data structures, as well as experience working with Java programming language. ",
-        "link": "https://github.com/WebXoX/Traveling-Sales-Man",
-        "image": "../img/tsm.png",        
-        "image2": "../img/java.png",
+        "discription":"The 42 IRC project involves creating an Internet Relay Chat (IRC) server using C++. I will build a server that allows users \
+                        to connect, join channels, and exchange messages. Key concepts include socket programming, protocol design, and \
+                        handling concurrency. The project emphasizes user authentication and real-time chat functionality. ",
+        "link": "https://github.com/WebXoX/irc",
+        "image": "../img/irc.png",        
+        "image2": "../img/c++.png",
         "show":true
     },
     {
         "id": 1010,
         "title": "Inception",
-        "discription":"I learned how to solve the traveling salesman problem using the 2-opt algorithm in Java. The 2-opt algorithm is a local search algorithm that iteratively improves a given tour of cities until a locally optimal solution is reached. The worst part of this process was preparing the data, which involves creating a distance matrix for the cities to be visited. The best part of this process was search, where I was able to apply the 2-opt algorithm to iteratively improve the tour and reach a locally optimal solution. Overall, this gave me a solid foundation in problem-solving with algorithms and data structures, as well as experience working with Java programming language. ",
-        "link": "https://github.com/WebXoX/Traveling-Sales-Man",
-        "image": "../img/tsm.png",        
-        "image2": "../img/java.png",
+        "discription":"The 42 Inception project focuses on system administration using Docker. I learned to virtualize Docker images within a \
+                        personal virtual machine. Key concepts include Docker Compose, Dockerfiles, and managing services like Nginx,   \
+                        WordPress, and MariaDB. This project taught me how to deploy my projects in a safer and more controlled environment \
+                        (version control). ",
+        "link": "https://github.com/WebXoX/Docker-learning/tree/inception",
+        "image": "../img/inception.png",        
+        "image2": "../img/docker.png",
         "show":true
     },
     {
         "id": 1011,
         "title": "Ft_Transcendence",
-        "discription":"I learned how to solve the traveling salesman problem using the 2-opt algorithm in Java. The 2-opt algorithm is a local search algorithm that iteratively improves a given tour of cities until a locally optimal solution is reached. The worst part of this process was preparing the data, which involves creating a distance matrix for the cities to be visited. The best part of this process was search, where I was able to apply the 2-opt algorithm to iteratively improve the tour and reach a locally optimal solution. Overall, this gave me a solid foundation in problem-solving with algorithms and data structures, as well as experience working with Java programming language. ",
-        "link": "https://github.com/WebXoX/Traveling-Sales-Man",
-        "image": "../img/tsm.png",        
-        "image2": "../img/java.png",
+        "discription":"transcendence marks the final project in Ecole 42 core curriculum, where students must \
+                        develop a full-stack web application that features a Pong game. The project requires    \
+                        students to integrate various modules into both the front and back end of the application,  \
+                        allowing for the incorporation of various technologies and features ",
+        "link": "https://github.com/WebXoX",
+        "image": "../img/pong.png",        
+        "image2": "../img/fullstack.png",
         "show":true
     }
 ]
